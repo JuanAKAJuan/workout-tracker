bc792be579a71c7fd021f8efa031c84e4de62aca
Juan Mireles <juanmireles@proton.me>
2025-02-28 21:23:30 -0500

    1. Add icons to bottom tabs. 2. Make the names of the tabs look better. 3. Remove the `(tabs)` route from appearing at the top of the screen. 4. Make the names of the tab routes look better at the top of the    screen.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,45 @@
 import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="settings" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="mesocycles"
+        options={{
+          title: "Mesocycles",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="bars" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="history" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="stats"
+        options={{
+          title: "Stats",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="bar-chart" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -1,9 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Settings() {
+export default function Stats() {
   return (
     <View style={styles.container}>
-      <Text>Settings</Text>
+      <Text>Stats</Text>
     </View>
   );
 }
